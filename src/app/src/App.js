eef6ce94a92efa046961300514f7a9c720fa693a
Eddy Ma <eddy.ma@smsmt.com>
2017-04-25 21:00:23 +1000

    Add home page

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import AboutMe from './pages/AboutMe';
+import ContactMe from './pages/Home';
 import ContactMe from './pages/ContactMe';
 import Blog from './pages/Blog';
 import Project from './pages/Project';
@@ -17,6 +18,7 @@ class App extends Component {
             <Menu />
             <Header />
             <div className="content row">
+            <Route exact path="/home" component={Home} />
               <Route exact path="/project" component={Project} />
               <Route exact path="/aboutme" component={AboutMe} />
               <Route exact path="/blog/:blogid?" component={Blog} />
---
+++ src/app/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home container">
+                <h3>I am a passionate developer want to learn everything</h3>
+                <p className="about-me-description">
+                    I love programming, i do it at work as well as my spare time
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
