2102325171532fda950afc43da09c4ecc3f6189c
Eddy Ma <eddy.ma@smsmt.com>
2017-06-16 16:30:58 +1000

    Code refactoring

--- src/app/src/components/GitHubRepo.js
+++ src/app/src/components/GitHubRepo.js
@@ -4,7 +4,6 @@ import './Footer.css';
 class GitHubRepo extends Component {
     
     render() {
-        console.log(this.props);
         return (
                 <li  key={this.props.html_url}>
                     <a href={this.props.html_url}>{this.props.name}</a><button onClick={() => this.props.onButtonClick()} >Click Me</button>
