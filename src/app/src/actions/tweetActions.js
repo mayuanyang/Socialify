3b1816e877fe87f127d7aef789e8984719908572
Eddy Ma <ocean_ltd@hotmail.com>
2017-07-01 13:08:50 +1000

    initial draft to integrate with twitter api

--- src/app/src/actions/tweetActions.js
+++ src/app/src/actions/tweetActions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import config from "../config";
 
 export function fetchTweets() {
   return function(dispatch) {
@@ -42,4 +43,28 @@ export function updateTweet(id, text) {
 
 export function deleteTweet(id) {
   return { type: 'DELETE_TWEET', payload: id}
+}
+
+export function fetchListThroughApi() {
+  dispatch({type: "FETCH_LIST_THROUGH_API"});
+  /* API Doc here
+    https://dev.twitter.com/rest/reference/get/collections/list
+  */
+
+  const AuthStr = `OAuth oauth_consumer_key="${config.twitter_api_consumer_key}",
+oauth_nonce="RandomNumberIsOk", 
+oauth_signature="", 
+oauth_signature_method="HMAC-SHA1", 
+oauth_timestamp="1318622958", 
+oauth_token="${config.twitter_api_access_token}", 
+oauth_version="1.0"
+ `;
+  
+  axios.get("https://api.twitter.com/1.1/collections/list.json", { 'headers': { 'Authorization': AuthStr } })
+        .then((response) => {
+          dispatch({type: "FETCH_LIST_FULFILLED", payload: response.data})
+        })
+        .catch((err) => {
+          dispatch({type: "FETCH_LIST_REJECTED", payload: err})
+        })
 }
