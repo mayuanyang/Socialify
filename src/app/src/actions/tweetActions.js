0e93b52aa39a75eec629609675328f25bb43d3aa
Eddy Ma <eddy.ma@octopus.com>
2017-07-08 17:03:28 +1000

    Disable twitter from home page as it does not support CORS

--- src/app/src/actions/tweetActions.js
+++ src/app/src/actions/tweetActions.js
@@ -3,19 +3,13 @@ import config from "../config";
 
 
 export function fetchTweets() {
+  return function(dispatch){
   dispatch({type: "FETCH_TWEETS"});
   /* API Doc here
     https://dev.twitter.com/rest/reference/get/collections/list
   */
 
-  const AuthStr = `OAuth oauth_consumer_key="${config.twitter_api_consumer_key}",
-oauth_nonce="RandomNumberIsOk", 
-oauth_signature="", 
-oauth_signature_method="HMAC-SHA1", 
-oauth_timestamp="1318622958", 
-oauth_token="${config.twitter_api_access_token}", 
-oauth_version="1.0"
- `;
+  const AuthStr = `OAuth oauth_consumer_key="${config.twitter_api_consumer_key}", oauth_nonce="RandomNumberIsOk", oauth_signature="", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1318622958", oauth_token="${config.twitter_api_access_token}", oauth_version="1.0"`;
   
   axios.get("https://api.twitter.com/1.1/collections/list.json", { 'headers': { 'Authorization': AuthStr } })
         .then((response) => {
@@ -23,5 +17,6 @@ oauth_version="1.0"
         })
         .catch((err) => {
           dispatch({type: "FETCH_TWEETS_REJECTED", payload: err})
-        })
+        });
+  }
 }
--- src/app/src/pages/Home.js
+++ src/app/src/pages/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
     componentWillMount() {
         this.props.dispatch(wordpress.fetchPosts());
         this.props.dispatch(github.fetchStarred());
-        this.props.dispatch(twitter.fetchTweets());
+        //this.props.dispatch(twitter.fetchTweets());
 
     }
 
